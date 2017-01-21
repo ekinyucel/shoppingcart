d771f13b817fc0445f8a9a0df072689356d2e6a8
Broke116 <ekiny018@gmail.com>
2017-01-21 11:50:53 +0300

    rest api init

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
+const api = require('./server/routes/api.js');
 const app = express();
 
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/api', api);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
